17917bab7ede0d455b6709e32bcbad1034bc125a
Konstantin Ivanov <kewogc@gmail.com>
2018-02-04 13:02:30 +0700

    Update nokogumbo.rb

--- lib/nokogumbo.rb
+++ lib/nokogumbo.rb
@@ -21,7 +21,9 @@ module Nokogiri
         string = reencode(string)
       end
 
-      Nokogumbo.parse(string.to_s, options[:max_parse_errors] || 0)
+      doc = Nokogumbo.parse(string.to_s, options[:max_parse_errors] || 0)
+      doc.encoding = 'UTF-8'
+      doc
     end
 
     # Fetch and parse a HTML document from the web, following redirects,
